9bba1bba31ad5f0a2ece2692230ae3a51d6838e0
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-05 16:30:00 +0530

    powerof2.C
    power of 2

---
+++ powerof2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int base,exponent;
+long double power=2;
+int i;
+printf("Enter base:");
+scanf("%d",&base);
+printf("Enter exponent:");
+scanf("%d",&exponent);
+for(i=1;i<=exponent;i++)
+{
+if(P=p*2)
+{
+printf("Yes");
+}
+else
+{
+printf("No");
+}
+}
+}
