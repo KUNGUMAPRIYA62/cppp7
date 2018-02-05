e5484ebaf433e0f489a08f5ef266fe73d321ad08
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-05 16:43:54 +0530

    avgnum.C

---
+++ avgnum.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,n,k,avg,sum;
+printf("enter the n values:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("enter the array of n values:");
+scanf("%d",&k[i]);
+}
+for(i=1;i<=n;i++)
+{
+sum=sum+k[i];
+}
+avg=sum/n;
+printf("avg:%d",avg);
+}
