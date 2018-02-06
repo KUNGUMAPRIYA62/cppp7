0ccdd99eed595815e4f07cf062cd76fcb5da621f
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-06 21:28:21 +0530

    concatenatestring.C
    concatenate

---
+++ concatenatestring.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char a[100],b[100];
+	printf("\n enter the first string");
+	scanf("%s",&a);
+  printf("\n enter the second string");
+  scanf("%s",&b);
+	printf("\n concatenated string is: %s%s",a,b);
+  }
