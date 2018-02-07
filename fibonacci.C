1ce023e033cd2c3931c48b1a83e5ddfdcc7720c5
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-07 16:47:15 +0530

    fibonacci.C
    fibonacci num

---
+++ fibonacci.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,i,terms;
+printf("enter number of terms:");
+scanf("%d",&terms);
+a=0;
+b=1;
+c=0;
+printf("fibonacci terms:");
+for(i=1;i<=terms;i++)
+{
+printf("%d",c);
+a=b;
+b=c;
+c=a+b;
+}
+}
