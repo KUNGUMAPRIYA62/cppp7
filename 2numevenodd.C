1df50275bd1d80394ca4929928089c7bfc2fa992
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-09 16:05:21 +0530

    2numevenodd.C
    sum of 2 num even or odd

---
+++ 2numevenodd.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+char even,odd;
+printf("Enter the two numbers:");
+scanf("%d%d",&a,&b);
+c=a+b;
+{
+printf("%d",c);
+}
+if(c%2==0)
+{
+printf("Number is even",even);
+}
+else
+{
+printf("Number is odd",odd);
+}
+}
