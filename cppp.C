42d12f39564d6cfe65a56e3069da3fd5b846dedf
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-09 16:59:19 +0530

    Create cppp.C

---
+++ cppp.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int i,n,k;
+printf("enter the string:");
+scanf("%s",&str);
+for(i=0;i<n;i++)
+{
+if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z'))
+   {
+     k=2
+     else((str[i]>='1'&&str[i]<='9'))
+     
